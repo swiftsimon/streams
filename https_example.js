5485c4a68edc423721806aff37f3db95e9ad2fe3
Simon <simon@simonhamilton.ca>
2018-01-20 21:29:23 +0000

    modified host and path to allow us to pull large data from Monty script, streamed un chunks

--- https_example.js
+++ https_example.js
@@ -2,9 +2,9 @@ var https = require('https');
 
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
-}
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
+};
 
 // called by https when the request is made.
 var callback = function(response) {
