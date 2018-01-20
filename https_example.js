07b48dc3fd97cad4299afe22aa910735bb41dec7
Simon <simon@simonhamilton.ca>
2018-01-20 21:26:22 +0000

    modify callback function to read the data

--- https_example.js
+++ https_example.js
@@ -1,6 +1,5 @@
 var https = require('https');
 
-console.log('I did it!');
 
 var options = {
   host: 'www.example.org',
@@ -10,15 +9,17 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk){
+    console.log('[--CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
+
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
 console.log("I've made the request!");
-
-
-
 
 
